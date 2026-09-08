c45cba4e59ce0beb029ec688b7ae55e20880d9a3
El-Duckster <elena.fredenbrink@gmail.com>
2026-09-08 23:28:44 +0200

    chore/add project skalette

---
+++ src/main/java/com/warehouse/WarehouseApplication.java
@@ -0,0 +1,15 @@
+package com.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Entry point for the Warehouse Management System (main)Starts an embedded web server and wires together the layered architecture:Controller -> Service -> Repository (in-memory)
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+       void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+
+    }
+}
