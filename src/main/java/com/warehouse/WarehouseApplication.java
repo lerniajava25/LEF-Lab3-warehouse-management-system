228237b8ee46f319ff5b4dfad5432543eac7cbd6
El-Duckster <elena.fredenbrink@gmail.com>
2026-09-09 09:18:34 +0200

    chore/edited

--- src/main/java/com/warehouse/WarehouseApplication.java
+++ src/main/java/com/warehouse/WarehouseApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WarehouseApplication {
 
-       void main(String[] args) {
+       public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
 
     }
